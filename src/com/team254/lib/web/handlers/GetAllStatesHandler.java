f0e6b030aa7389e04adc11b49838c28c3dfd2d37
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2015-01-12 13:44:54 -0800

    Swaps old gson for JSONObj for states handler

--- src/com/team254/lib/web/handlers/GetAllStatesHandler.java
+++ src/com/team254/lib/web/handlers/GetAllStatesHandler.java
@@ -2,6 +2,7 @@ package com.team254.lib.web.handlers;
 
 import java.io.IOException;
 import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import com.google.gson.Gson;
+import org.json.simple.JSONObject;
+
 import com.team254.lib.util.SystemManager;
 
 public class GetAllStatesHandler extends AbstractHandler {
@@ -17,14 +19,13 @@ public class GetAllStatesHandler extends AbstractHandler {
   public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
 
-    Gson gson = new Gson();
     response.setContentType("application/json;charset=utf-8");
     response.setStatus(HttpServletResponse.SC_OK);
     baseRequest.setHandled(true);
 
-    HashMap<String, String> states = SystemManager.get();
-    String res = gson.toJson(states);
-    response.getWriter().println(res);
+    HashMap<String, String> s = SystemManager.getInstance().get();
+    JSONObject json = new JSONObject(s);
+    response.getWriter().println(json.toJSONString());
   }
 
 }
