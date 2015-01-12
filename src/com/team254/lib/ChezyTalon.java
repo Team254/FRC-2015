3c184b87bfdef3fee58c5310bcf39f98e30bc588
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2015-01-12 09:50:34 -0800

    Converts systemManager's functions and class vars to static

--- src/com/team254/lib/ChezyTalon.java
+++ src/com/team254/lib/ChezyTalon.java
@@ -6,20 +6,19 @@ import com.team254.lib.util.SystemManager;
 import edu.wpi.first.wpilibj.Talon;
 
 public class ChezyTalon extends Talon implements Serializable {
-  public String name;
-  
+  private String name;
+
   public ChezyTalon(int channel, String name) {
     super(channel);
     this.name = name;
     SystemManager.getInstance().add(this);
   }
 
-  @Override
   public String serialize() {
     double val = this.get();
     return Double.toString(val);
   }
-  
+
   public String getName() {
     return this.name;
   }
--- src/com/team254/lib/util/SystemManager.java
+++ src/com/team254/lib/util/SystemManager.java
@@ -25,38 +25,37 @@ public class SystemManager {
     this.sysmap = new Hashtable<String, Serializable>();
   }
 
-  public void add(Serializable v) {
+  public static void add(Serializable v) {
     sysmap.put(v.getName(), v);
   }
 
   // Returns a map of all states
-  public HashMap<String, String> get() {
+  public static HashMap<String, String> get() {
     HashMap<String, String> states = new HashMap<String, String>();
 
-    Collection<String> keys = this.sysmap.keySet();
+    Collection<String> keys = sysmap.keySet();
 
     for (String k : keys) {
-      String v = this.sysmap.get(k).serialize();
+      String v = sysmap.get(k).serialize();
       states.put(k, v);
     }
     return states;
   }
 
-  public HashMap<String, String> get(String k) {
+  public static HashMap<String, String> get(String k) {
     HashMap<String, String> state = new HashMap<String, String>();
     state.put(k, sysmap.get(k).serialize());
     return state;
   }
-  
+
   // Returns a map of states for the devices specified in args
-  public HashMap<String, String> get(String []args) {
+  public static HashMap<String, String> get(String []args) {
     HashMap<String, String> states = new HashMap<String, String>();
     for (String k : args) {
-      String v = this.sysmap.get(k).serialize();
+      String v = sysmap.get(k).serialize();
       states.put(k, v);
     }
     return states;
   }
-
 
 }
