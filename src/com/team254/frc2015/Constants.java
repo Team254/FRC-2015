37315b8c9f0dc587b5c770687e8843283fb54370
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-01-24 20:07:07 -0800

    Add Constants file

---
+++ src/com/team254/frc2015/Constants.java
@@ -0,0 +1,12 @@
+package com.team254.frc2015;
+
+import com.team254.lib.util.ConstantsBase;
+
+public class Constants extends ConstantsBase {
+	public static double kTestGain = 0.1;
+
+	@Override
+	public String getFileLocation() {
+		return "~/constants.txt";
+	}
+}
