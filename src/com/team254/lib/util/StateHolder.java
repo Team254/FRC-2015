09e6320f42a6ee39bde508e1ac308b5c057bb232
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-02-10 12:19:26 -0800

    Add StateHolder

---
+++ src/com/team254/lib/util/StateHolder.java
@@ -0,0 +1,20 @@
+package com.team254.lib.util;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class StateHolder {
+	private HashMap<String, Object> states = new  HashMap<String, Object>();
+	
+	public void put(String key, Object value) {
+		states.put(key, value);
+	}
+	
+	public Object get(String key) {
+		return states.get(key);
+	}
+
+	public Set<String> keySet() {
+		return states.keySet();
+	}
+}
