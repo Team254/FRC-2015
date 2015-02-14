916969e904409ef3b399eb260667e80008c4e355
Leigh Pauls <leighpauls@fb.com>
2015-02-14 01:44:18 -0800

    Refactor drive controllers to use composition rather than inheritance

--- src/com/team254/frc2015/subsystems/Drive.java
+++ src/com/team254/frc2015/subsystems/Drive.java
@@ -2,10 +2,8 @@ package com.team254.frc2015.subsystems;
 
 import com.team254.frc2015.Constants;
 import com.team254.frc2015.subsystems.controllers.DriveStraightController;
-import com.team254.frc2015.subsystems.controllers.TrajectoryFollowingPositionController;
 import com.team254.lib.trajectory.TrajectoryFollower;
 import com.team254.lib.util.CheesySpeedController;
-import com.team254.lib.util.Controller;
 import com.team254.lib.util.DriveSignal;
 import com.team254.lib.util.Loopable;
 import com.team254.lib.util.Pose;
@@ -16,12 +14,18 @@ import com.team254.lib.util.gyro.GyroThread;
 import edu.wpi.first.wpilibj.Encoder;
 
 public class Drive extends Subsystem implements Loopable {
+
+    public interface DriveController {
+        DriveSignal update(Pose pose);
+        Pose getCurrentSetpoint();
+    }
+
 	public CheesySpeedController m_left_motor;
 	public CheesySpeedController m_right_motor;
 	public Encoder m_left_encoder;
 	public Encoder m_right_encoder;
 	public GyroThread m_gyro;
-	protected Controller m_controller = null;
+	private DriveController m_controller = null;
 
 	protected final double m_inches_per_tick = Constants.kDriveWheelSizeInches
 			* Math.PI / Constants.kElevatorEncoderCountsPerRev;
@@ -44,70 +48,43 @@ public class Drive extends Subsystem implements Loopable {
 
 	public void setOpenLoop(DriveSignal signal) {
 		m_controller = null;
-		set(signal);
+		setDriveOutputs(signal);
 	}
 
-	public synchronized TrajectoryFollower.TrajectorySetpoint getSetpoint() {
-		TrajectoryFollower.TrajectorySetpoint setpoint;
-		// Rather than reading encoder velocity, we report the last commanded
-		// velocity from a velocity profile. This ensures that the input is
-		// smooth when changing setpoints.
-		if (m_controller instanceof TrajectoryFollowingPositionController) {
-			setpoint = ((TrajectoryFollowingPositionController) m_controller)
-					.getSetpoint();
-		} else {
-			setpoint = new TrajectoryFollower.TrajectorySetpoint();
-			setpoint.pos = (getPose().getLeftDistance() + getPose().getRightDistance()) / 2.0;
-		}
-		return setpoint;
+	public synchronized void setDistanceSetpoint(double distance) {
+		setDistanceSetpoint(distance, Constants.kDriveMaxSpeedInchesPerSec);
 	}
 
-	public synchronized void setDistanceSetpoint(double setpoint) {
-		setDistanceSetpoint(setpoint, Constants.kDriveMaxSpeedInchesPerSec);
-	}
-
-	public synchronized void setDistanceSetpoint(double setpoint, double velocity) {
+	public synchronized void setDistanceSetpoint(double distance, double velocity) {
 		// 0 < vel < max_vel
 		double vel_to_use = Math.min(Constants.kDriveMaxSpeedInchesPerSec, Math.max(velocity, 0));
-		TrajectoryFollower.TrajectorySetpoint prior_setpoint = getSetpoint();
-		if (!(m_controller instanceof DriveStraightController)) {
-			TrajectoryFollower.TrajectoryConfig config = new TrajectoryFollower.TrajectoryConfig();
-			config.dt = Constants.kControlLoopsDt;
-			config.max_acc = Constants.kDriveMaxAccelInchesPerSec2;
-			config.max_vel = vel_to_use;
-			m_controller = new DriveStraightController(
-					Constants.kDrivePositionKp, Constants.kDrivePositionKi,
-					Constants.kDrivePositionKd, Constants.kDrivePositionKv,
-					Constants.kDrivePositionKa, Constants.kDriveStraightKp,
-					Constants.kDriveStraightKi, Constants.kDriveStraightKd,
-					Constants.kDriveOnTargetError, config);
-		}
-		DriveStraightController controller = ((DriveStraightController) m_controller);
-		TrajectoryFollower.TrajectoryConfig config = controller.getConfig();
-		config.max_vel = vel_to_use;
-		controller.setConfig(config);
-		controller.setGoal(prior_setpoint, setpoint);
+
+        Pose currentSetpoint;
+        if (m_controller != null) {
+            // taking over from a previous controller, continue from its most recent desired state
+            currentSetpoint = m_controller.getCurrentSetpoint();
+        } else {
+            // first closed-loop action, continue from the robot's current physical state
+            currentSetpoint = getPhysicalPose();
+        }
+
+        TrajectoryFollower.TrajectoryConfig config = new TrajectoryFollower.TrajectoryConfig();
+        config.dt = Constants.kControlLoopsDt;
+        config.max_acc = Constants.kDriveMaxAccelInchesPerSec2;
+        config.max_vel = vel_to_use;
+        DriveStraightController driveStraightController = new DriveStraightController(
+                Constants.kDrivePositionKp, Constants.kDrivePositionKi,
+                Constants.kDrivePositionKd, Constants.kDrivePositionKv,
+                Constants.kDrivePositionKa, Constants.kDriveStraightKp,
+                Constants.kDriveStraightKi, Constants.kDriveStraightKd,
+                Constants.kDriveOnTargetError, config);
+        driveStraightController.setGoal(currentSetpoint, distance);
+        m_controller = driveStraightController;
 	}
 
-	private void set(DriveSignal signal) {
-		setLeftRight(signal.leftMotor, signal.rightMotor);
-	}
-
-	private void setLeftRight(double left, double right) {
-		m_left_motor.set(left);
-		m_right_motor.set(-right);
-	}
-
-	public Pose getPose() {
-		m_cached_pose.reset(
-                m_left_encoder.getDistance(),
-				m_right_encoder.getDistance(),
-                m_left_encoder.getRate(),
-                m_right_encoder.getRate(),
-				m_gyro.getAngle(),
-                m_gyro.getRate());
-		return m_cached_pose;
-	}
+    public synchronized void setTurnSetPoint(double heading, double max_velocity) {
+        throw new RuntimeException("Implement me");
+    }
 
 	@Override
 	public void getState(StateHolder states) {
@@ -116,20 +93,39 @@ public class Drive extends Subsystem implements Loopable {
         states.put("left_encoder_rate", m_left_encoder.getRate());
         states.put("right_encoder_rate", m_right_encoder.getRate());
         states.put("right_encoder", m_right_encoder.getDistance());
-        states.put("drive_set_point_pos", getSetpoint().pos);
-        // states.put("drive_set_point_velocity", getSetpoint().vel);
+
+        Pose setPointPose = m_controller == null
+                ? getPhysicalPose()
+                : m_controller.getCurrentSetpoint();
+        states.put(
+                "drive_set_point_pos",
+                DriveStraightController.encoderDistance(setPointPose));
 	}
 
 	@Override
 	public void update() {
-		if (m_controller instanceof DriveStraightController) {
-			DriveStraightController position_controller = (DriveStraightController) m_controller;
-            Pose pose = getPose();
-            DriveSignal curSig = position_controller.update(pose);
-			set(curSig);
-		} else {
-			// do nothing.
-		}
+        if (m_controller == null) {
+            return;
+        }
+        setDriveOutputs(m_controller.update(getPhysicalPose()));
 	}
 
+    private void setDriveOutputs(DriveSignal signal) {
+        m_left_motor.set(signal.leftMotor);
+        m_right_motor.set(-signal.rightMotor);
+    }
+
+    /**
+     * @return The pose according to the current sensor state
+     */
+    private Pose getPhysicalPose() {
+        m_cached_pose.reset(
+                m_left_encoder.getDistance(),
+                m_right_encoder.getDistance(),
+                m_left_encoder.getRate(),
+                m_right_encoder.getRate(),
+                m_gyro.getAngle(),
+                m_gyro.getRate());
+        return m_cached_pose;
+    }
 }
--- src/com/team254/frc2015/subsystems/controllers/DriveStraightController.java
+++ src/com/team254/frc2015/subsystems/controllers/DriveStraightController.java
@@ -1,41 +1,77 @@
 package com.team254.frc2015.subsystems.controllers;
 
+import com.team254.frc2015.subsystems.Drive;
+import com.team254.lib.trajectory.TrajectoryFollower;
 import com.team254.lib.util.DriveSignal;
 import com.team254.lib.util.Pose.RelativePoseGenerator;
 import com.team254.lib.util.SynchronousPID;
 import com.team254.lib.trajectory.TrajectoryFollower.TrajectoryConfig;
 import com.team254.lib.util.Pose;
 
-public class DriveStraightController extends
-		TrajectoryFollowingPositionController {
+import static com.team254.lib.trajectory.TrajectoryFollower.TrajectorySetpoint;
 
-	private RelativePoseGenerator m_relative_pose_generator;
-	private SynchronousPID pid;
+public class DriveStraightController implements Drive.DriveController {
 
-	public DriveStraightController(double kp, double ki, double kd, double kv,
-			double ka, double kpTurn, double kiTurn, double kdTurn,
+    private TrajectoryFollowingPositionController mDistanceController;
+    private SynchronousPID mTurnPid;
+    private Pose mSetpointRelativePose;
+
+	public DriveStraightController(
+            double kpDist, double kiDist, double kdDist, double kvDist, double kaDist,
+            double kpTurn, double kiTurn, double kdTurn,
 			double on_target_delta, TrajectoryConfig config) {
-		super(kp, ki, kd, kv, ka, on_target_delta, config);
-		this.pid = new SynchronousPID();
-		pid.setPID(kpTurn, kiTurn, kdTurn);
+        mDistanceController = new TrajectoryFollowingPositionController(
+                kpDist, kiDist, kdDist, kvDist, kaDist, on_target_delta, config);
+		this.mTurnPid = new SynchronousPID();
+		mTurnPid.setPID(kpTurn, kiTurn, kdTurn);
 	}
 
-	public void setStartPose(Pose pose) {
-		m_relative_pose_generator = pose.new RelativePoseGenerator();
-	}
-
+    @Override
 	public DriveSignal update(Pose currentPose) {
-		if (m_relative_pose_generator == null) {
-            m_relative_pose_generator = currentPose.new RelativePoseGenerator();
-		}
-
-        double distance = (currentPose.getLeftDistance() + currentPose.getRightDistance()) / 2.0;
-        double velocity = (currentPose.getLeftVelocity() + currentPose.getRightVelocity()) / 2.0;
-        update(distance, velocity);
-		double throttle = get();
-		double turn = pid.calculate(currentPose.getHeading());
+        mDistanceController.update(
+                (currentPose.getLeftDistance() + currentPose.getRightDistance()) / 2.0,
+                (currentPose.getLeftVelocity() + currentPose.getRightVelocity()) / 2.0);
+		double throttle = mDistanceController.get();
+		double turn = mTurnPid.calculate(currentPose.getHeading());
 
 		return new DriveSignal(throttle + turn, throttle - turn);
 	}
 
+    @Override
+    public Pose getCurrentSetpoint() {
+        TrajectorySetpoint trajectorySetpoint = mDistanceController.getSetpoint();
+        double dist = trajectorySetpoint.pos;
+        double velocity = trajectorySetpoint.vel;
+        return new Pose(
+                mSetpointRelativePose.getLeftDistance() + dist,
+                mSetpointRelativePose.getRightDistance() + dist,
+                mSetpointRelativePose.getLeftVelocity() + velocity,
+                mSetpointRelativePose.getRightVelocity() + velocity,
+                0,
+                0);
+    }
+
+    public void setGoal(Pose priorSetpoint, double goalSetpoint) {
+        TrajectorySetpoint initialSetpoint = new TrajectorySetpoint();
+        initialSetpoint.pos = encoderDistance(priorSetpoint);
+        initialSetpoint.vel = encoderVelocity(priorSetpoint);
+        initialSetpoint.acc = 0;
+        mDistanceController.setGoal(initialSetpoint, goalSetpoint);
+        mTurnPid.setSetpoint(priorSetpoint.getHeading());
+        mSetpointRelativePose = new Pose(
+                priorSetpoint.getLeftDistance(),
+                priorSetpoint.getRightDistance(),
+                0,
+                0,
+                priorSetpoint.getHeading(),
+                priorSetpoint.getHeadingVelocity());
+    }
+
+    public static double encoderVelocity(Pose pose) {
+        return (pose.getLeftVelocity() + pose.getRightVelocity()) / 2.0;
+    }
+
+    public static double encoderDistance(Pose pose) {
+        return (pose.getLeftDistance() + pose.getRightDistance()) / 2.0;
+    }
 }
