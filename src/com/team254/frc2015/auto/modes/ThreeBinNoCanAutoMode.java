4f52afb6dad3b164b1d27e66e8d9abfdaedad389
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-03-01 13:56:03 -0800

    Comment auto mode a bit

--- src/com/team254/frc2015/auto/modes/ThreeBinNoCanAutoMode.java
+++ src/com/team254/frc2015/auto/modes/ThreeBinNoCanAutoMode.java
@@ -13,12 +13,11 @@ public class ThreeBinNoCanAutoMode extends AutoMode {
     protected void routine() throws AutoModeEndedException {
         intake.open();
 
-
+        // Get first can out of way, drive slowly to 2nd tote
         top_carriage.setPositionSetpoint(Constants.kTopCarriageMaxPositionInches, true);
         drive.setDistanceSetpoint(70, 30);
         waitTime(.7);
         bottom_carriage.setPositionSetpoint(18, true);
-
         waitForDrive(5.0);
 
         // Intake 2nd tote
@@ -33,59 +32,61 @@ public class ThreeBinNoCanAutoMode extends AutoMode {
         intake.open();
         bottom_carriage.setPositionSetpoint(24, true);
 
-        intake.setSpeed(.6);
-
         // Knock can
+        intake.setSpeed(.6);
         drive.setDistanceSetpoint(115, 30);
         waitTime(1.1);
 
+        // Close intake on can
         intake.close();
         intake.setSpeed(0);
         waitForDrive(2.0);
 
+        // Turn with can intake and open
         drive.setTurnSetPoint(Math.PI/3.0);
         waitTime(.5);
         intake.open();
-
         waitForDrive(.25);
 
-
+        // Turn back to center line
         drive.setTurnSetPoint(0);
         waitForDrive(1.5);
 
+        // Drive to last tote
         drive.reset();
         drive.setDistanceSetpoint(40);
         bottom_carriage.setPositionSetpoint(18, true);
         intake.open();
 
-
+        // Grab 3rd tote
         waitForDrive(2.0);
         intake.setSpeed(1.0);
         intake.close();
-        drive.setTurnSetPoint(Math.PI / 3.0);
 
+        // Turn to face auto zone
+        drive.setTurnSetPoint(Math.PI / 3.0);
         waitForDrive(1.2);
 
+        // Drive to auto zone
         drive.reset();
         drive.setDistanceSetpoint(137);
         intake.close();
         intake.setSpeed(0.0);
-        // Index 3rd tote
+
+        // Index 3rd tote while driving
         bottom_carriage.setPositionSetpoint(1, false);
         waitForCarriage(bottom_carriage, 2.0);
-
-
         bottom_carriage.setPositionSetpoint(10, true);
-
         waitForDrive(3.0);
 
+        // Turn back to horizontal
         drive.setTurnSetPoint(0);
         bottom_carriage.setPositionSetpoint(1, true);
         waitForCarriage(bottom_carriage, 2.0);
         waitForDrive(1.0);
 
+        // Let go of stack and drive away
         bottom_carriage.setFlapperOpen(true);
-
         drive.reset();
         drive.setDistanceSetpoint(-60);
         intake.open();
