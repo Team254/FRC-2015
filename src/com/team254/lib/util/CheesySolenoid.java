2a4d0b8b22e012738fb6a3dc60f1ddb0e93fbae4
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-03-03 12:47:55 -0800

    Cache solenoid state
    The setter for this is 2 ipcs, no need to call it at 50hz

--- src/com/team254/lib/util/CheesySolenoid.java
+++ src/com/team254/lib/util/CheesySolenoid.java
@@ -1,11 +1,23 @@
 package com.team254.lib.util;
 
+import com.team254.frc2015.Robot;
 import edu.wpi.first.wpilibj.Solenoid;
 
 public class CheesySolenoid extends Solenoid {
+    private boolean m_on = false;
+    private boolean m_was_disabled = true;
 
     public CheesySolenoid(int channel) {
         super((channel > 7 ? 1 : 0), (channel > 7 ? channel - 8 : channel));
     }
 
+    @Override
+    public void set(boolean on) {
+        boolean is_enabled = Robot.getState() != Robot.RobotState.DISABLED;
+        if ((is_enabled && m_was_disabled) || on != m_on) {
+            super.set(on);
+        }
+        m_on = on;
+        m_was_disabled = Robot.getState() == Robot.RobotState.DISABLED;
+    }
 }
