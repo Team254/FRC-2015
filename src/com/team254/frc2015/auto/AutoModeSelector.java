d885aa05e082a554e713e80a9a9d3ba91a46c93e
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-03-21 14:08:06 -0700

    Remove unused code

--- src/com/team254/frc2015/auto/AutoModeSelector.java
+++ src/com/team254/frc2015/auto/AutoModeSelector.java
@@ -22,6 +22,7 @@ public class AutoModeSelector {
 	}
 	
 	public AutoModeSelector() {
+        registerAutonomous(new TestBangBangAutoMode());
         registerAutonomous(new ThreeBinNoCanAutoMode());
         registerAutonomous(new DoNothingAutoMode());
 	}
