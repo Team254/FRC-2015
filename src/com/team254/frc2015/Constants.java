dca585e44f0b0b5b6193a89c587229edb6e47599
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-03-26 20:20:06 -0700

    Update constants

--- src/com/team254/frc2015/Constants.java
+++ src/com/team254/frc2015/Constants.java
@@ -36,18 +36,18 @@ public class Constants extends ConstantsBase {
     public static double kDriveStraightKd = 0;
     public static double kDrivePositionKv = 0.008;
     public static double kDrivePositionKa = 0.0017;
-    public static double kDriveOnTargetError = 1.0;
+    public static double kDriveOnTargetError = 0.75;
     public static double kDrivePathHeadingFollowKp = 0.01;
 
     // TurnInPlaceController gains
     public static double kTurnMaxSpeedRadsPerSec = 5.25;
     public static double kTurnMaxAccelRadsPerSec2 = 5.25;
-    public static double kTurnKp = 4.0;
-    public static double kTurnKi = 0.13;
-    public static double kTurnKd = 0.0;
-    public static double kTurnKv = 0.11;
-    public static double kTurnKa = 0.05;
-    public static double kTurnOnTargetError = 0.025;
+    public static double kTurnKp = 3.0;
+    public static double kTurnKi = 0.18;
+    public static double kTurnKd = 0.23;
+    public static double kTurnKv = 0.085;
+    public static double kTurnKa = 0.075;
+    public static double kTurnOnTargetError = 0.0225;
 
     // Height presets
     public static double kCoopTopHeight = 49.75;
