2a2825b3261b26a92e45410cd99fa84f183dbe8b
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-03-27 23:11:30 -0700

    Speed up can grabbing

--- src/com/team254/frc2015/behavior/routines/CanGrabRoutine.java
+++ src/com/team254/frc2015/behavior/routines/CanGrabRoutine.java
@@ -51,26 +51,18 @@ public class CanGrabRoutine extends Routine {
                 }
                 break;
             case MOVE_CARRIAGES:
-                setpoints.pivot_action = RobotSetpoints.TopCarriagePivotAction.PIVOT_UP;
+                setpoints.pivot_action = RobotSetpoints.TopCarriagePivotAction.PIVOT_DOWN;
                 setpoints.claw_action = RobotSetpoints.TopCarriageClawAction.CLOSE;
                 setpoints.intake_action = RobotSetpoints.IntakeAction.OPEN;
                 if (m_is_new_state) {
                     setpoints.m_elevator_setpoints.bottom_setpoint = Optional.of(2.0);
                     setpoints.m_elevator_setpoints.top_setpoint = Optional.of(6.5);
                 }
-                if (!m_is_new_state  && bottom_carriage.isOnTarget() && top_carriage.isOnTarget()) {
-                    new_state = States.ROTATE_DOWN;
+                if (!m_is_new_state  && bottom_carriage.isOnTarget() && top_carriage.isOnTarget() && m_state_timer.get() > .5) {
+                    new_state = States.OPEN_GRABBER;
                 } else if (!m_is_new_state && m_state_timer.get() > 2.3) {
                     setpoints.bottom_open_loop_jog = Optional.of(0.0);
                     setpoints.top_open_loop_jog = Optional.of(0.0);
-                    new_state = States.ROTATE_DOWN;
-                }
-                break;
-            case ROTATE_DOWN:
-                setpoints.pivot_action = RobotSetpoints.TopCarriagePivotAction.PIVOT_DOWN;
-                setpoints.claw_action = RobotSetpoints.TopCarriageClawAction.CLOSE;
-                setpoints.intake_action = RobotSetpoints.IntakeAction.OPEN;
-                if (m_state_timer.get() > .55) {
                     new_state = States.OPEN_GRABBER;
                 }
                 break;
